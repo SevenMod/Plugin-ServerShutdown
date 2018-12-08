561506f39c30ffaae591569c9c41db920ef3f6f7
Steve Guidetti <stevotvr@gmail.com>
2018-12-08 10:46:52 -0800

    Initial commit

---
+++ Plugin-ServerShutdown/ServerShutdown.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="ServerShutdown.cs" company="Steve Guidetti">
+// Copyright (c) Steve Guidetti. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace SevenMod.Plugin.ServerShutdown
+{
+    using SevenMod.Core;
+
+    /// <summary>
+    /// Plugin that schedules automatic server shutdowns and enables shutdown votes.
+    /// </summary>
+    public sealed class ServerShutdown : PluginAbstract
+    {
+        /// <inheritdoc/>
+        public override PluginInfo Info => new PluginInfo
+        {
+            Name = "ServerShutdown",
+            Author = "SevenMod",
+            Description = "Schedules automatic server shutdowns and enables shutdown votes.",
+            Version = "0.1.0.0",
+            Website = "https://github.com/SevenMod/Plugin-ServerShutdown"
+        };
+
+        /// <inheritdoc/>
+        public override void OnLoadPlugin()
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Server Shutdown
+
+Plugin for SevenMod that schedules automatic server shutdowns and enables shutdown votes.
+
+## License
+
+The source code for SevenMod is available under the terms of the [MIT License](https://github.com/SevenMod/Plugin-ServerShutdown/blob/master/LICENSE.txt).
+See the LICENSE.txt in the project root for details.
