04ab5f36b4ece2d61a126ffa8f6316750a09e353
Steve Guidetti <stevotvr@gmail.com>
2019-02-06 16:08:25 -0800

    Update plugin version format

--- Plugin-ServerShutdown/ServerShutdown.cs
+++ Plugin-ServerShutdown/ServerShutdown.cs
@@ -75,7 +75,7 @@ namespace SevenMod.Plugin.ServerShutdown
             Name = "ServerShutdown",
             Author = "SevenMod",
             Description = "Schedules automatic server shutdowns and enables shutdown votes.",
-            Version = "0.1.0.0",
+            Version = "0.1.0",
             Website = "https://github.com/SevenMod/Plugin-ServerShutdown"
         };
 
